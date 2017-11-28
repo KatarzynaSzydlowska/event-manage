25a8fc9ca539570b918a9ead1b503bda3a7171c6
Konrad Wadas <konrad.wadas3@gmail.com>
2017-11-28 19:59:27 +0100

    implement db model for event and tests for many-to-many relationship

---
+++ cosine/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.conf import settings
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    date = models.DateTimeField()
+    description = models.CharField(max_length=1000)
+    spots = models.IntegerField()
+    location = models.CharField(max_length=200)  # TODO,  change to some location framework
+    price = models.FloatField()
+    enrollment_begin = models.DateTimeField()
+    enrollment_end = models.DateTimeField()
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="owner")
+    participants = models.ManyToManyField(settings.AUTH_USER_MODEL)

6dc7761d3bedd8816ff90dd96617ba8e1c91aa6e
Katarzyna Szydlowska <kszydlowska11@gmail.com>
2017-12-06 19:38:25 +0100

    implement event addition form

--- cosine/forms.py
+++ cosine/forms.py
@@ -1,20 +1,25 @@
 from django.contrib.auth.models import User
 from django import forms
+from .models import Event
+from django.contrib.admin.widgets import AdminDateWidget
+import datetime
+
 
 class LoginForm(forms.Form):
     username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)    
+    password = forms.CharField(widget=forms.PasswordInput)
+
 
 class RegistrationForm(forms.ModelForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     email = forms.CharField()
-    password = forms.CharField(label = "Password", widget=forms.PasswordInput)
-    password2 = forms.CharField(label = "Retype password", widget=forms.PasswordInput)
+    password = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(label="Retype password", widget=forms.PasswordInput)
 
     class Meta:
         model = User
-        help_texts = { 'username': None,}
+        help_texts = {'username': None, }
         fields = ['username', 'first_name', 'last_name', 'email']
 
     def clean_password2(self):
@@ -22,3 +27,18 @@ class RegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError("Passwords don't match!")
         return cd['password2']
+
+
+class EventForm(forms.ModelForm):
+    name = forms.CharField()
+    date = forms.DateTimeField(widget=forms.widgets.DateInput(attrs={'class': 'datetimepicker'}))
+    description = forms.CharField()
+    spots = forms.IntegerField()
+    location = forms.CharField()  # TODO,  change to some location framework
+    price = forms.FloatField()
+    enrollment_begin = forms.DateTimeField(widget=forms.widgets.DateInput(attrs={'class': 'datetimepicker'}))
+    enrollment_end = forms.DateTimeField(widget=forms.widgets.DateInput(attrs={'class': 'datetimepicker'}))
+
+    class Meta:
+        model = Event
+        fields = ['name', 'date', 'description', 'spots', 'location', 'price', 'enrollment_begin', 'enrollment_end']

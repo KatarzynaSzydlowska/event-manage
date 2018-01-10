745b003badbfa3223f24866cbc8328b70c9b38e4
Dominik Kasperski <kasperski.dominik@gmail.com>
2018-01-10 12:24:41 +0100

    basic mailing

--- mailing_engine/urls.py
+++ mailing_engine/urls.py
@@ -4,5 +4,5 @@ from . import views
 app_name = 'mailing_engine'
 urlpatterns = [
     url(r'^event/(?P<event_id>[0-9]+)/send_info$', views.send_info, name='send_info'),
-    url(r'^event/(?P<event_id>[0-9]+)/send_info_participant$', views.send_info_participant, name='send_info_participant'),
+    # url(r'^event/(?P<event_id>[0-9]+)/send_info_participant$', views.send_info_participant, name='send_info_participant'),
 ]

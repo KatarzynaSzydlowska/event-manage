4d2b2f5af476f9a165b984d0385f43c69ad31abb
Konrad Wadas <konrad.wadas3@gmail.com>
2018-01-13 16:34:06 +0100

    remove obsolete model

--- mailing_engine/views.py
+++ mailing_engine/views.py
@@ -38,7 +38,7 @@ def send_info(request, event_id):
         "Thank You for using COSINE!"
 
         message = EmailMessage(subject, body, from_email=event.owner.email, bcc=[request.user.email])
-        message.attach("QR", event.qr_code.read())
+        message.attach("QR", event.qr_code)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
 
@@ -61,7 +61,7 @@ def send_info(request, event_id):
         "Owner's email address:\t" + event.owner.email + " \n " +\
         "Thank You for using COSINE!"
         message = EmailMessage(subject, body, from_email=event.owner.email, bcc=bcc_participants)
-        message.attach("QR", event.qr_code.read())
+        message.attach("QR", event.qr_code)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
     return HttpResponseForbidden("Only owner can send email with information to all participants!")
@@ -84,7 +84,7 @@ def send_message(request, event_id):
                 "Owner's email address:\t" + event.owner.email + " \n " +\
                 "Thank You for using COSINE"
                 message = EmailMessage(subject, body, from_email=event.owner.email, bcc=bcc_participants)
-                message.attach("QR", event.qr_code.read())
+                message.attach("QR", event.qr_code)
                 message.send()                
                 return redirect(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
     return render(request, 'mailing_engine/send_message.html', {'mail_form': form})

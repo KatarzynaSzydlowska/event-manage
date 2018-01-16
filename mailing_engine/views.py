24120715d7da53853430efd9c936ab59b980cc03
Dominik Kasperski <kasperski.dominik@gmail.com>
2018-01-16 13:49:09 +0100

    mailing almost repaired

--- mailing_engine/views.py
+++ mailing_engine/views.py
@@ -20,10 +20,10 @@ def send_info(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     if request.user in event.participants.all():
 
-        subject = event.name + ' ' + 'information'
+        msubject = event.name + ' ' + 'information'
         from_email = event.owner.email
         subject = 'Information about ' + event.name
-        body = "Dear " + request.user.first_name + "! \n "\
+        mbody = "Dear " + request.user.first_name + "! \n "\
         + "There are requested informations: \n " +\
         "What:\t" + event.name + ". \n " +\
         "When:\t" + str(event.date) + ". \n " +\
@@ -31,23 +31,23 @@ def send_info(request, event_id):
         "Enrollment finish at \t" + str(event.enrollment_end) + ". \n " +\
         "Where:\t" + str(event.location) + " .\n "
         if event.price>0:
-            body = body + "Price:\t" + str(event.price) + ". \n "
-        body = body + "Additional information: \n " + event.description + " \n " +\
+            mbody = mbody + "Price:\t" + str(event.price) + ". \n "
+        mbody = mbody + "Additional information: \n " + event.description + " \n " +\
         "Link to an event in COSINE:\t" + 'http://event-manage-pite.herokuapp.com/event/'+str(event.id) + " \n " +\
         "Owner's email address:\t" + event.owner.email + " \n " +\
         "Thank You for using COSINE!"
 
-        message = EmailMessage(subject, body, from_email=event.owner.email, bcc=[request.user.email])
-        message.attach("QR", event.qr_code)
+        message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=[request.user.email])
+        message.attach_file(event.qr_code.path)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
 
     if request.user.id == event.owner.id:
-        subject = event.name + ' ' + 'information'
+        msubject = event.name + ' ' + 'information'
         from_email = event.owner.email
         bcc_participants = [part.email for part in event.participants.all()]
         subject = 'Information about ' + event.name
-        body = "Dear " + request.user.first_name + "! \n "\
+        mbody = "Dear " + request.user.first_name + "! \n "\
         + "There are requested informations: \n " +\
         "What:\t" + event.name + ". \n " +\
         "When:\t" + str(event.date) + ". \n " +\
@@ -55,13 +55,13 @@ def send_info(request, event_id):
         "Enrollment finish at \t" + str(event.enrollment_end) + ". \n " +\
         "Where:\t" + str(event.location) + " .\n "
         if event.price>0:
-            body = body + "Price:\t" + str(event.price) + ". \n "
-        body = body + "Additional information: \n " + event.description + " \n " +\
+            mbody = mbody + "Price:\t" + str(event.price) + ". \n "
+        mbody = mbody + "Additional information: \n " + event.description + " \n " +\
         "Link to an event in COSINE:\t" + 'http://event-manage-pite.herokuapp.com/event/'+str(event.id) + " \n " +\
         "Owner's email address:\t" + event.owner.email + " \n " +\
         "Thank You for using COSINE!"
-        message = EmailMessage(subject, body, from_email=event.owner.email, bcc=bcc_participants)
-        message.attach("QR", event.qr_code)
+        message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=bcc_participants)
+        message.attach_file(event.qr_code.path)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
     return HttpResponseForbidden("Only owner can send email with information to all participants!")
@@ -70,21 +70,22 @@ def send_info(request, event_id):
 def send_message(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     if request.user.id == event.owner.id:
-        if request.method == 'GET':
+        if request.method == 'POST':
             form = ContactForm()
         else:
             form = ContactForm(request.POST)
             if form.is_valid():
-                subject = form.cleaned_data['subject']
-                body = form.cleaned_data['message']
+                msubject = form.cleaned_data['subject']
+                mbody = form.cleaned_data['message']
                 bcc_participants = [part.email for part in event.participants.all()]
-                body = body + \
+                mbody = mbody + \
                 "Link to an event in COSINE:\t" +\
                  'http://event-manage-pite.herokuapp.com/event/'+str(event.id) + " \n " +\
                 "Owner's email address:\t" + event.owner.email + " \n " +\
                 "Thank You for using COSINE"
-                message = EmailMessage(subject, body, from_email=event.owner.email, bcc=bcc_participants)
-                message.attach("QR", event.qr_code)
+                print(type(mbody))
+                message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=bcc_participants)
+                message.attach_file(event.qr_code.path)
                 message.send()                
                 return redirect(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
-    return render(request, 'mailing_engine/send_message.html', {'mail_form': form})
+        return render(request, 'mailing_engine/send_message.html', {'mail_form': form})

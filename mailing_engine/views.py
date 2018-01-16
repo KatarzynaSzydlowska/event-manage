75559b2abc47e558188dcf665509f77b751bfea9
Konrad Wadas <konrad.wadas3@gmail.com>
2018-01-16 21:15:00 +0100

    fix

--- mailing_engine/views.py
+++ mailing_engine/views.py
@@ -38,7 +38,8 @@ def send_info(request, event_id):
         "Thank You for using COSINE!"
         message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=[request.user.email])
 
-        meta,f =event.qr_code.file._storage.client.files_download(event.qr_code.file.name)
+        meta,f =event.qr_code.file._storage.client.dbx.files_download_to_file(event.qr_code.file.name,"."+event.qr_code.file.name)
+        f=open("."+event.qr_code.file.name,'rb')
         message.attach("QR.png", f.read())
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})

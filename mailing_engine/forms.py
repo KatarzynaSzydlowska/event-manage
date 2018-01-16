accaab4d9739e67dbf93cf5f4d8c33f1959176eb
Dominik Kasperski <kasperski.dominik@gmail.com>
2018-01-16 22:42:21 +0100

    fixed emails and CET time on heroku

--- mailing_engine/forms.py
+++ mailing_engine/forms.py
@@ -5,21 +5,6 @@ from django.contrib.admin.widgets import AdminDateWidget
 import datetime
 
 
-
-class MessageForm(forms.Form):
-    subject = forms.CharField()
-    body = forms.CharField()
-
-    class Meta:
-        help_texts = {'body': None}
-        fields = ['subject','body']
-            
-        
-
 class ContactForm(forms.Form):
     subject = forms.CharField(required=True)
-    message = forms.CharField(widget=forms.Textarea)
-
-    class Meta:
-        help_texts = {'body': None}
-        fields = ['subject','message']
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- mailing_engine/views.py
+++ mailing_engine/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic import View
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
@@ -11,9 +11,7 @@ from .forms import ContactForm
 import mailing_engine
 
 from django.core.mail import EmailMessage
-from django.core.mail import send_mail
-
-# Create your views here.
+from django.core.mail import send_mail, BadHeaderError
 
 @login_required
 def send_info(request, event_id):
@@ -37,7 +35,8 @@ def send_info(request, event_id):
         "Owner's email address:\t" + event.owner.email + " \n " +\
         "Thank You for using COSINE!"
         message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=[request.user.email])
-        message.attach("QR.png", event.qr_code.url)
+        response = event.qr_code.file._storage.client.files_download(event.qr_code.file.name)
+        message.attach("QR.png", response[1].content)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
 
@@ -45,7 +44,6 @@ def send_info(request, event_id):
         msubject = event.name + ' ' + 'information'
         from_email = event.owner.email
         bcc_participants = [part.email for part in event.participants.all()]
-        subject = 'Information about ' + event.name
         mbody = "Dear " + request.user.first_name + "! \n "\
         + "There are requested informations: \n " +\
         "What:\t" + event.name + ". \n " +\
@@ -60,7 +58,8 @@ def send_info(request, event_id):
         "Owner's email address:\t" + event.owner.email + " \n " +\
         "Thank You for using COSINE!"
         message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=bcc_participants)
-        message.attach("QR.png", event.qr_code.url)
+        response = event.qr_code.file._storage.client.files_download(event.qr_code.file.name)
+        message.attach("QR.png", response[1].content)
         message.send()
         return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
     return HttpResponseForbidden("Only owner can send email with information to all participants!")
@@ -69,7 +68,7 @@ def send_info(request, event_id):
 def send_message(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     if request.user.id == event.owner.id:
-        if request.method == 'POST':
+        if request.method == 'GET':
             form = ContactForm()
         else:
             form = ContactForm(request.POST)
@@ -78,13 +77,14 @@ def send_message(request, event_id):
                 mbody = form.cleaned_data['message']
                 bcc_participants = [part.email for part in event.participants.all()]
                 mbody = mbody + \
-                "Link to an event in COSINE:\t" +\
+                "\n Link to an event in COSINE:\t" +\
                  'http://event-manage-pite.herokuapp.com/event/'+str(event.id) + " \n " +\
                 "Owner's email address:\t" + event.owner.email + " \n " +\
                 "Thank You for using COSINE"
-                print(type(mbody))
                 message = EmailMessage(subject=msubject, body=mbody, from_email=event.owner.email, bcc=bcc_participants)
-                message.attach("QR.png", event.qr_code.url)
-                message.send()                
-                return redirect(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
+                response = event.qr_code.file._storage.client.files_download(event.qr_code.file.name)
+                message.attach("QR.png", response[1].content)
+                message.send()
+                return render(request, 'mailing_engine/send_info.html', {'event': event,'user':request.user})
         return render(request, 'mailing_engine/send_message.html', {'mail_form': form})
+    return HttpResponseForbidden("Only owner can send email to all participants!")
